e74c06a7c2b0ddeccddda704e2e754e0ed77b885
Stefano <stet@mit.edu>
2017-12-03 20:37:26 -0500

    Add files via upload

--- RGOT.py
+++ RGOT.py
@@ -24,12 +24,6 @@ from bokeh.resources import CDN
 from bokeh.palettes import Spectral7, Blues
 from bokeh.models import HoverTool, ColumnDataSource
 
-
-
-
-num_of_turns=1500
-num_of_turns
-
 #-------------------------------------------------------------------------------
 # The following functions plays one istance of the multi-armed bandit algorithm
 #-------------------------------------------------------------------------------
@@ -55,13 +49,14 @@ def RGOT(G,number_of_turns = 1500, number_of_arms = 10, \
             update_arm(arms[arm_to_play],x_t,t,policy)                          # update the arm performance under this policy
     return pd.DataFrame(arms), rewards_history, tradeoff_history
 #-------------------------------------------------------------------------------
-RGOT(G,number_of_turns = 200, number_of_arms = 10, \
-    arms_behavior = "Bernoulli", policies = ["variable_pool", \
-        "Epsilon_z_greedy", "Epsilon_greedy", "Epsilon_soft_greedy", \
-         "UCB_z" , "UCB_soft", "UCB"])
 
-number_of_games = 15
+#Example:
+num_of_turns=1500
 G = set_G_as(num_of_turns,"Wave")
+RGOT(G,number_of_turns = 200, number_of_arms = 10, arms_behavior = "Bernoulli", policies = ["variable_pool", "Epsilon_z_greedy", "Epsilon_greedy", "Epsilon_soft_greedy", "UCB_z" , "UCB_soft", "UCB"])
+
+
+
 
 
 #-------------------------------------------------------------------------------
@@ -86,88 +81,10 @@ def compare_algorithms(number_of_games,  G, number_of_turns = 1000, number_of_ar
     return cumulative_rewards, trade_off
 
 
-
-
-
-
-# to try:
-g = set_G_as(num_of_turns,"Wave")
-
 #-------------------------------------------------------------------------------
 
-
+# Example:
 cumulative_rewards, trade_off = compare_algorithms(number_of_games,  G, number_of_turns = 1500, number_of_arms = 50,
      policies = [ "variable_pool", \
     "Epsilon_z_greedy", "Epsilon_greedy", "Epsilon_soft_greedy", \
     "UCB_z" , "UCB_soft", "UCB"])
-
-
-#-------------------------------------------------------------------------------
-# to show Histogram:
-ndf = policies_performance(cumulative_rewards2)
-output_file("means.html")
-hist = Bar(ndf, values='mean', title="Average cumulative reward for each policy",legend=None,color=Spectral7)
-show(hist)
-# OLD
-def policies_performance(cumulative_rewards): # needs numpy and pandas
-    # returns a dataframe where the indexes are the policies and the columns are
-    # mean and standard deviation of the cululative rewards
-    x=[]
-    for policy in cumulative_rewards.keys():
-        x.append([np.mean(cumulative_rewards[policy]), \
-        np.std(cumulative_rewards[policy])   ])
-    return pd.DataFrame(np.array(x),columns=['mean', 'std'], index = cumulative_rewards.keys() )
-#-------------------------------------------------------------------------------
-
-
-# good examples:
-#cumulative_rewards, trade_off = compare_algorithms(number_of_games,  G, number_of_turns = 1500, number_of_arms = 50,
-#     policies = [ "variable_pool", \
-#    "Epsilon_z_greedy", "Epsilon_greedy", "Epsilon_soft_greedy", \
-#    "UCB_z" , "UCB_soft", "UCB"])
-# cumulative_rewards2 = compare_algorithms(30, g, number_of_turns = 400, number_of_arms=70, policies = ["UCB_z" , "UCB_soft", "UCB","variable_pool" ])
-# cumulative_rewards2 = compare_algorithms(30, g, number_of_turns = 400, number_of_arms=70, policies = ["UCB_z" , "UCB_soft", "UCB","variable_pool","Epsilon_z_greedy", "Epsilon_greedy", "Epsilon_soft_greedy" ])
-
-
-
-#-------------------------------------------------------------------------------
-# Plotting
-#-------------------------------------------------------------------------------
-
-hist = Histogram(df['total_pulls_Epsilon_greedy'], title="total_pulls_Epsilon_greedy" )
-show(hist)
-
-df2=pd.DataFrame(CR)
-p = Bar(df2, 'arm_id', values='total_pulls_Epsilon_greedy', title="Tot")
-show(p)
-
-p = Bar(df, 'arm_id', values='total_pulls_Epsilon_greedy', title="Tot")
-show(p)
-
-b = Bar(df, 'arm_id', values='mean_reward_Epsilon_greedy', title="Tot", legend=None)
-show(b)
-
-c = Bar(df, 'arm_id', values='rewards_Epsilon_greedy', agg='sum' , title="Tot")
-show(c)
-
-
-len([i for i in range(num_of_turns)])
-###
-from bokeh.plotting import figure, output_file, show
-pp = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-pp.legend.location='bottom_left'
-pp.line([i for i in range(num_of_turns)], r, legend="Temp.", line_width=2)
-show(pp)
-html = file_html(pp, CDN, "my plot")
-html
-
-
-    # compute bound
-
-import matplotlib.cm as cm
-colormap =cm.get_cmap("jet")
-different_colors=7
-color_mapping=colormap(np.linspace(0,1,different_colors),1,True)
-bokeh_palette=["#%02x%02x%02x" % (r, g, b) for r, g, b in color_mapping[:,0:3]]
-
-    # compute betas to compare with 1/m
